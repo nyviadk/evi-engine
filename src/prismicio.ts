1a45aad0c6e1acd83f5a959db9f88f47137d7f13
nyviadk <nyvia.dk@gmail.com>
2026-06-22 12:47:41 +0200

    Upgraded prismic cli to Type Builder

---
+++ src/prismicio.ts
@@ -0,0 +1,25 @@
+import { createClient as baseCreateClient } from "@prismicio/client";
+import { enableAutoPreviews, type CreateClientConfig } from "@prismicio/next/pages";
+import prismicConfig from "../prismic.config.json";
+
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName = prismicConfig.repositoryName;
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config - Configuration for the Prismic client.
+ */
+export const createClient = ({ previewData, req, ...config }: CreateClientConfig = {}) => {
+	const client = baseCreateClient(repositoryName, {
+		routes: prismicConfig.routes,
+		...config,
+	});
+
+	enableAutoPreviews({ client, previewData, req });
+
+	return client;
+};
